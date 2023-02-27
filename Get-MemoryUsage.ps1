e21b938a7d49f9f3f3aea623666989f9c34a59a8
Keanan Paul <keananpaul@yahoo.com>
2023-02-27 09:39:15 -0800

    Removed redundant TotalMemory variable.

--- Get-MemoryUsage.ps1
+++ Get-MemoryUsage.ps1
@@ -20,13 +20,7 @@ function Get-MemoryUsage()
     $Entry = ""
     while (@("q", "quit") -notcontains $Entry.ToLower())
     {
-        $TotalMemory = 0
         $Programs = Get-Process
-        foreach ($Program in $Programs)
-        {
-            $TotalMemory += $Program.WorkingSet
-        }
-
         $FormattedPrograms = @()
         $MemoryTotal = 0
         ($Programs | Sort-Object -Descending -Property WorkingSet) | ForEach-Object {
@@ -43,7 +37,7 @@ function Get-MemoryUsage()
         $FormattedPrograms | Format-Table -Property Name, Memory, MemTotal, Title, Path
 
         Write-Host "Total usage: " -NoNewline -ForegroundColor Yellow
-        Write-Host (Format-Size $TotalMemory) -ForegroundColor White
+        Write-Host (Format-Size $MemoryTotal) -ForegroundColor White
         Write-Host "Press enter to run again or type Q to quit."
         Write-Host "> " -NoNewline
         $Entry = Read-Host
